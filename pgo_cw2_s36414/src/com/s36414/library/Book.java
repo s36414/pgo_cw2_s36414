f2e9ea61be2ab8d3d1519374fe15e0535d3cbe23
Pawel Cala <p.cala@futuremind.com>
2026-04-10 18:22:45 +0200

    Add Main, Book classes

---
+++ pgo_cw2_s36414/src/com/s36414/library/Book.java
@@ -0,0 +1,48 @@
+package com.s36414.library;
+
+import java.util.Objects;
+
+public class Book {
+
+    private final String isbn;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private boolean available;
+
+    public Book(String isbn, String title, String author, int publicationYear) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.available = true;
+    }
+
+    public String getIsbn() { return isbn; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public int getPublicationYear() { return publicationYear; }
+    public boolean isAvailable() { return available; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthor(String author) { this.author = author; }
+    public void setPublicationYear(int publicationYear) { this.publicationYear = publicationYear; }
+    public void setAvailable(boolean available) { this.available = available; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(isbn); }
+
+    @Override
+    public String toString() {
+        return String.format("Book{isbn='%s', title='%s', author='%s', year=%d, available=%s}",
+                isbn, title, author, publicationYear, available);
+    }
+}
---
+++ pgo_cw2_s36414/src/com/s36414/library/Main.java
@@ -0,0 +1,13 @@
+package com.s36414.library;
+
+public class Main {
+    static void main(String[] args) {
+        Book book = new Book(
+                "0140449264",
+                "The Count of Monte Cristo",
+                "Alexandre Dumas",
+                2003);
+
+        System.out.println(book);
+    }
+}
